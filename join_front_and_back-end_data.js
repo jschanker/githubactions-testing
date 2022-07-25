5a41febe30d70fd31f43c7aba6a475583e848a33
jschanker <jschanker@molloy.edu>
2022-07-25 02:36:28 -0400

    Use promises

--- join_front_and_back-end_data.js
+++ join_front_and_back-end_data.js
@@ -7,44 +7,44 @@ const axios = require('axios');
   // const frontEndPathwayData = JSON.parse(core.getInput('front_end_data'));
   // const backEndPathwayData = core.getInput('back_end_data').pathways || [];
   
-  const frontEndFilePath = process.argv[2];
-  const backEndFilePath = process.argv[3]; 
+const frontEndFilePath = process.argv[2];
+const backEndFilePath = process.argv[3];
   
-  const feResponse = await axios(frontEndFilePath);
-  const beResponse = await axios(backEndFilePath);
-  
-  if (!feResponse?.data || !beResponse?.data) {
-    throw new Error('Failed to retrieve front-end or back-end data');
-  }
-  
-  const frontEndPathwayData = feResponse.data;
-  const backEndPathwayData = beResponse.data.pathways || [];
-  
-  const feCodeToIndexMap = frontEndPathwayData.reduce(
-      (codeMap, pathway, index) => {
-        if (pathway.code) {
-          codeMap[pathway.code] = index;
-        }
-        return codeMap;
-      },
-      {}
-    );
-  const joinedPathwayData = backEndPathwayData.reduce(
-    (pathwayData, pathway) => {
-      const indexOfPathway = feCodeToIndexMap[pathway.code];
-      if (indexOfPathway != undefined) {
-        pathwayData[indexOfPathway] = {
-          ...pathway,
-          ...pathwayData[indexOfPathway],
-        };
-      } else {
-        pathwayData.push(pathway);
-      }
-      return pathwayData;
-    },
-    frontEndPathwayData
-  );
-  core.setOutput('joined_pathway_data', JSON.stringify(joinedPathwayData));
-// } catch(e) {
-//  core.setFailed(e.message);
-// }
+  // const feResponse = await axios(frontEndFilePath);
+  // const beResponse = await axios(backEndFilePath);
+
+axios(frontEndFilePath).then((feResponse) => {
+  axios(backEndFilePath).then((beResponse) => {
+    if (!feResponse?.data || !beResponse?.data) {
+      core.setFailed('Failed to retrieve front-end or back-end data');
+    } else {
+      const frontEndPathwayData = feResponse.data || [];
+      const backEndPathwayData = beResponse.data.pathways || [];
+      const feCodeToIndexMap = frontEndPathwayData.reduce(
+          (codeMap, pathway, index) => {
+            if (pathway.code) {
+              codeMap[pathway.code] = index;
+            }
+            return codeMap;
+          },
+          {}
+        );
+      const joinedPathwayData = backEndPathwayData.reduce(
+        (pathwayData, pathway) => {
+          const indexOfPathway = feCodeToIndexMap[pathway.code];
+          if (indexOfPathway != undefined) {
+            pathwayData[indexOfPathway] = {
+              ...pathway,
+              ...pathwayData[indexOfPathway],
+            };
+          } else {
+            pathwayData.push(pathway);
+          }
+          return pathwayData;
+        },
+        frontEndPathwayData
+      );
+      core.setOutput('joined_pathway_data', JSON.stringify(joinedPathwayData));        
+    }
+  }).catch((e) => core.setFailed(e.message));
+}).catch((e) => core.setFailed(e.message));
