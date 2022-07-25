915802bd03225105038498d558e161f44cc83c98
jschanker <jschanker@molloy.edu>
2022-07-25 02:23:56 -0400

    Use file data in joining pathway data instead

--- join_front_and_back-end_data.js
+++ join_front_and_back-end_data.js
@@ -1,10 +1,25 @@
 const core = require('@actions/core');
+const axios = require('axios');
 
 try {
-  console.log("FE", core.getInput('steps.join_pathway_data.outputs.front_end_data'));
-  console.log("BE", core.getInput('steps.join_pathway_data.outputs.back_end_data'));
-  const frontEndPathwayData = JSON.parse(core.getInput('front_end_data'));
-  const backEndPathwayData = core.getInput('back_end_data').pathways || [];
+  // console.log("FE", core.getInput('steps.join_pathway_data.outputs.front_end_data'));
+  // console.log("BE", core.getInput('steps.join_pathway_data.outputs.back_end_data'));
+  // const frontEndPathwayData = JSON.parse(core.getInput('front_end_data'));
+  // const backEndPathwayData = core.getInput('back_end_data').pathways || [];
+  
+  const frontEndFilePath = process.argv[2];
+  const backEndFilePath = process.argv[3];
+  
+  const feResponse = await axios(frontEndFilePath);
+  const beResponse = await axios(backEndFilePath);
+  
+  if (!feResponse?.data || !beResponse?.data) {
+    throw new Error('Failed to retrieve front-end or back-end data');
+  }
+  
+  const frontEndPathwayData = feResponse.data;
+  const backEndPathwayData = beResponse.data.pathways || [];
+  
   const feCodeToIndexMap = frontEndPathwayData.reduce(
       (codeMap, pathway, index) => {
         if (pathway.code) {
